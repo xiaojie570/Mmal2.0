f910da9fac1c20fab0523542fd89938043013a56
fuxiaojie <457732323@qq.com>
2018-12-11 15:20:24 +0800

    RedisPool RedisPoolUtil

--- src/main/java/com/mmall/common/RedisPool.java
+++ src/main/java/com/mmall/common/RedisPool.java
@@ -1,16 +1,21 @@
 package com.mmall.common;
 
 import com.mmall.util.PropertiesUtil;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * Created by lenovo on 2018/12/10.
  */
 public class RedisPool {
-    private static RedisPool pool ;
+    private static JedisPool pool ;  // jedis连接池
     private static Integer maxTotal = Integer.parseInt(PropertiesUtil.getProperty("redis.max.total","20"));
     private static Integer minIdel = Integer.parseInt(PropertiesUtil.getProperty("redis.min.idel","2"));
     private static Integer maxIdel = Integer.parseInt(PropertiesUtil.getProperty("redis.max.idel","10"));
+
+    private static String redisIp = PropertiesUtil.getProperty("redis.ip");
+    private static Integer redisPort = Integer.parseInt(PropertiesUtil.getProperty("redis.port"));
 
     private static Boolean testOnBorrow = Boolean.parseBoolean(PropertiesUtil.getProperty("redis.test.on.borrow"));
     private static Boolean testOnReturn =  Boolean.parseBoolean(PropertiesUtil.getProperty("redis.test.on.retur"));
@@ -26,6 +31,33 @@ public class RedisPool {
 
         config.setBlockWhenExhausted(true); // 当连接耗尽的时候，是否阻塞，false会抛出异常，true阻塞直到超时，默认为true。
 
+        pool = new JedisPool(config,redisIp,redisPort);
     }
 
+    static {
+        initPool();
+    }
+
+    // 获取连接池中的数据
+    public static Jedis getJedis() {
+        return pool.getResource();
+    }
+
+    //
+
+    public static void returnResource(Jedis jedis) {
+        pool.returnResource(jedis);
+    }
+
+    public  static void returnBrokenResource(Jedis jedis) {
+        pool.returnBrokenResource(jedis);
+    }
+
+    public static void main(String[] args) {
+        RedisPool p = new RedisPool();
+        Jedis jedis = p.getJedis();
+        jedis.set("sc","NEU");
+        p.returnResource(jedis);
+        System.out.println("program end....");
+    }
 }
---
+++ src/main/java/com/mmall/util/RedisPoolUtil.java
@@ -0,0 +1,98 @@
+package com.mmall.util;
+
+import com.mmall.common.RedisPool;
+import lombok.extern.slf4j.Slf4j;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * Created by lenovo on 2018/12/11.
+ */
+@Slf4j
+public class RedisPoolUtil {
+    public static String set(String key, String value) {
+        Jedis jedis = null;
+        String result = null;
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.set(key,value);
+        } catch (Exception e) {
+            log.error("set key:{} value:{} error",key,value,e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    public static String get(String key) {
+        Jedis jedis = null;
+        String result = null;
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.get(key);
+        } catch (Exception e) {
+            log.error("get key:{} error",key,e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    public static String setEx(String key,String value,int exTime) {
+        Jedis jedis = null;
+        String result = null;
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.setex(key,exTime,value);
+
+        } catch (Exception e) {
+            log.info("setEx key:{} error",key);
+            RedisPool.returnResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    public static Long expire(String key, int exTime) {
+        Jedis jedis = null;
+        Long result = null;
+
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.expire(key,exTime);
+        } catch (Exception e) {
+            log.info("expire key:{} exTime:{} error",key,exTime);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    public static Long del(String key) {
+        Jedis jedis = null;
+        Long result = null;
+        try {
+            jedis = RedisPool.getJedis();
+            result = jedis.del(key);
+        } catch (Exception e) {
+            log.info("del key:{} error",key, e);
+            RedisPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisPool.returnResource(jedis);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        RedisPoolUtil.set("age","35");
+        String result = RedisPoolUtil.get("age");
+        RedisPoolUtil.setEx("sex","nv",60*10);
+        RedisPoolUtil.expire("age",60*5);
+        RedisPoolUtil.del("age");
+
+    }
+}
