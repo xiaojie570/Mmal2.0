2cbf9a46678f3818d97ea3c5a2608fdc50539f72
root <root@ip-219-216-64-145.neu.edu.cn>
2018-12-13 09:21:47 -0800

    CookieUtil

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -6,6 +6,7 @@ import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
 import com.mmall.util.JsonUtil;
 import com.mmall.util.RedisPoolUtil;
 import lombok.Setter;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -36,10 +39,15 @@ public class UserController {
      */
     @RequestMapping(value="login.do",method = RequestMethod.POST)
     @ResponseBody
-    public ServerResponse<User> login(String username, String password, HttpSession session) {
+    public ServerResponse<User> login(String username,
+                                      String password,
+                                      HttpSession session,
+                                      HttpServletRequest httpServletRequest,
+                                      HttpServletResponse httpServletResponse) {
         ServerResponse<User> response = iUserService.login(username,password);
         // 如果返回值是正确的，则将用户信息存储在session中
         if(response.isSuccess()) {
+            CookieUtil.writeLoginToken(httpServletResponse,session.getId());
             //session.setAttribute(Const.CURRENT_USER,response.getData());
             // Cookie: JSESSIONID=AF3E1E5DF03A8D1F629750BDAC5CA288
             // A0CEAD9BE1AF0C363C9FC0AC504FCC01
--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -1,12 +1,67 @@
 package com.mmall.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 public class CookieUtil {
     private final static String COOKIE_DOMAIN = ".tomcat.com";
     private final static String COOLIE_NAME="mmall_login_token";
 
-    public static void WriteLoginToken(HttpServletResponse response,String token) {
+    /**
+     * 写入登录 cookie
+     * @param response
+     * @param token
+     */
+    public static void writeLoginToken(HttpServletResponse response,String token) {
+        Cookie cookie = new Cookie(COOLIE_NAME,token);
+        cookie.setDomain(COOKIE_DOMAIN);
+        // 单位是秒，这里设置有效期为一年。
+        // 如果这个MaxAge不设置的话，不会写入硬盘，而是写在内存。只在当前页面有效
+        cookie.setMaxAge(60 * 60 * 24 * 365);
+        // 代表设置在根目录
+        cookie.setPath("/");
+        log.info("write cookieName:{}, cookieValue:{}",cookie.getName(),cookie.getValue());
+        response.addCookie(cookie);
+    }
 
+    /**
+     * 读取cookie信息
+     */
+    public static String readLoginToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for(Cookie cookie: cookies) {
+                log.info("read cookieName:{}, cookieValue:{}",cookie.getName(),cookie.getValue());
+                if(StringUtils.equals(cookie.getName(),COOLIE_NAME)) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 注销的时候需要删除 cookie
+     * 删除的时候又要读又要写
+     */
+    public static void delLoginToken(HttpServletRequest request,HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for(Cookie cookie : cookies) {
+                if(StringUtils.equals(cookie.getName(),COOLIE_NAME)) {
+                    log.info("del cookieName:{},cookieValue:{}",cookie.getName(),cookie.getValue());
+                    cookie.setPath("/");
+                    cookie.setDomain(COOKIE_DOMAIN);
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                    return;
+                }
+            }
+        }
     }
 }
